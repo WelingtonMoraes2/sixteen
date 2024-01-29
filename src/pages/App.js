696f61faaecb7c569e694e85543e95f492701186
Welington Moraes <53630451+WelingtonMoraes@users.noreply.github.com>
2024-01-29 14:40:44 -0300

    config do projeto

---
+++ src/pages/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import "../styles/App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>teste 1</h2>
+      <div className="buttons">
+        <button class="button is-primary">Primary</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
